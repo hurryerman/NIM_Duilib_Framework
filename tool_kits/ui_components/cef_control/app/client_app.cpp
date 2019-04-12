508a18cb594cf9ed5b3443bbec37bd4acaa36b8a
牛牛2050 <hurryerman@qq.com>
2019-04-12 14:47:33 +0800

    加载ppflash插件; 32.0.0.171;

--- tool_kits/ui_components/cef_control/app/client_app.cpp
+++ tool_kits/ui_components/cef_control/app/client_app.cpp
@@ -24,8 +24,8 @@ void ClientApp::OnBeforeCommandLineProcessing(const CefString& process_type, Cef
 	// Pass additional command-line flags to the browser process.
 	if (process_type.empty()) 
 	{
-		command_line->AppendSwitchWithValue("ppapi-flash-version", "20.0.0.228");
-		command_line->AppendSwitchWithValue("ppapi-flash-path", "PepperFlash\\pepflashplayer.dll");
+        command_line->AppendSwitchWithValue("--ppapi-flash-version", "32.0.0.171");
+        command_line->AppendSwitchWithValue("--ppapi-flash-path", "PepperFlash\\pepflashplayer.dll");
 
 		//同一个域下的使用同一个渲染进程
 		command_line->AppendSwitch("process-per-site");
